e9c22ef49a1a8d775d52d0a95eab8e4d4f31c679
tanvee-gujral <gujral_tanvi@yahoo.com>
2017-07-22 14:48:06 +0530

    first commit

--- app/index.js
+++
@@ -1,9 +0,0 @@
-import jquery from 'jquery';
-import UIkit from 'uikit';
-import Icons from 'react-uikit-icons';
-
-// loads the Icon plugin
-UIkit.use(Icons);
-
-// components can be called from the imported UIkit reference
-UIkit.notification('Hello world.');
